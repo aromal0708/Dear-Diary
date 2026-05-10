a3ccf1bcf4dd74a1da143dbcff2b4e27e8184594
Aromal0708 <aromalsunil12@gmail.com>
2026-05-10 10:04:39 +0530

    debug: debug login page not protected error

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -3,13 +3,18 @@ import { authOptions } from "@/app/lib/auth";
 import { signOut, useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 
-const Dashboard =  () => {
+const Dashboard = () => {
   const session = useSession();
   console.log("Session:", session);
+
+  const { data, status } = session;
   return (
     <div>
       <h1>Dashboard</h1>
-      <button onClick={() => signOut()}>signOut</button>
+
+      <p>Welcome, {data?.user?.name}</p>
+      <p>Status: {status}</p>
+      <button className="cursor-pointer outline-1 " onClick={() => signOut()}>signOut</button>
     </div>
   );
 };
--- proxy.ts
+++ proxy.ts
@@ -4,11 +4,16 @@ import type { NextRequest } from "next/server";
 
 export default async function proxy(req: NextRequest) {
   const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+  const { pathname } = req.nextUrl;
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
+
+  if (token && pathname === "/login") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/dashboard"] };
+export const config = { matcher: ["/dashboard", "/login"] };
