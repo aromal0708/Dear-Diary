6d5523dd456604a4ae4cbc9892f6d94f533110af
Aromal0708 <aromalsunil12@gmail.com>
2026-05-11 21:51:53 +0530

    fix: add signup to protected pages

--- proxy.ts
+++ proxy.ts
@@ -13,7 +13,10 @@ export default async function proxy(req: NextRequest) {
   if (token && pathname === "/login") {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
+  if (token && pathname === "/signup") {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
+  }
   return NextResponse.next();
 }
 
-export const config = { matcher: ["/dashboard", "/login"] };
+export const config = { matcher: ["/dashboard", "/login", "/signup"] };
